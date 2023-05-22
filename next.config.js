3906edd51064e80e360cb92de9a18514153bb64c
kou72 <genpokopokopoko@gmail.com>
2023-05-23 06:17:56 +0900

    GitHub Actionsでのデプロイに対応するため、next.config.jsを修正
    next.config.js
    - basePathを設定するために、process.env.GITHUB_ACTIONSがtrueの場合に"/open-calm-chat"を設定する
    - セミコロンを追加

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.GITHUB_ACTIONS && "/open-calm-chat",
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
