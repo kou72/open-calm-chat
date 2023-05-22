fcd4003da5999abe29e5579ed38b35f4a80aab9a
kou72 <genpokopokopoko@gmail.com>
2023-05-23 06:51:21 +0900

    _document.tsxファイルを新規作成、publicディレクトリに画像ファイルを追加
    _document.tsx
    - Next.jsのDocumentコンポーネントを拡張するために、import文で必要なコンポーネントを読み込む
    - title要素に「open-calm-chat」という文字列を設定
    - meta要素のog:imageプロパティに空文字列を設定
    
    public/open-calm-chat.png
    - 新規追加された画像ファイル

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>open-calm-chat</title>
+        <meta property="og:image" content="" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
