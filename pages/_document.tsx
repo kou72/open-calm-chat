6425406fde16ba56c6f8f3dd81b661a6ea55d28d
kou72 <genpokopokopoko@gmail.com>
2023-05-28 18:35:36 +0900

    Twitterカードの設定を追加
    _document.tsx
    - Twitterカードの設定を追加
      - カードの種類(summary)
      - サイトのTwitterアカウント(@kou7273)
      - タイトル(open-calm-chat)
      - 説明(open-calm-chat)
      - 画像(https://raw.githubusercontent.com/kou72/open-calm-chat/main/public/open-calm-chat.png)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,14 @@ export default function Document() {
           name="twitter:image"
           content="https://raw.githubusercontent.com/kou72/open-calm-chat/main/public/open-calm-chat.png"
         />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@kou7273" />
+        <meta name="twitter:title" content="open-calm-chat" />
+        <meta name="twitter:description" content="open-calm-chat" />
+        <meta
+          name="twitter:image"
+          content="https://raw.githubusercontent.com/kou72/open-calm-chat/main/public/open-calm-chat.png"
+        />
       </Head>
       <body>
         <Main />
