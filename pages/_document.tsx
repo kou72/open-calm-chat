77dc7c273479cd290c54b82588baae614e4c85ef
kou72 <genpokopokopoko@gmail.com>
2023-05-28 19:49:00 +0900

    _document.tsxからmetaタグを削除
    _document.tsx
    - metaタグを削除

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,10 +9,6 @@ export default function Document() {
           property="og:image"
           content="https://raw.githubusercontent.com/kou72/open-calm-chat/main/public/open-calm-chat.png"
         />
-        <meta
-          name="twitter:image"
-          content="https://raw.githubusercontent.com/kou72/open-calm-chat/main/public/open-calm-chat.png"
-        />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@kou7273" />
         <meta name="twitter:title" content="open-calm-chat" />
